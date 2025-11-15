db2c7a959c793470b37eec44bb41c21b7d34e673
marcellintacite <tacite.bahiga@gmail.com>
2025-11-15 10:02:54 +0300

    First commit

--- sidepanel/firebase.js
+++ sidepanel/firebase.js
@@ -8,13 +8,7 @@ import { getAI, getGenerativeModel, GoogleAIBackend, InferenceMode } from "fireb
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAXe7FQb5YVNmGjotq74EK2rRTPUmvt628",
-  authDomain: "gospelfamily-b59a7.firebaseapp.com",
-  projectId: "gospelfamily-b59a7",
-  storageBucket: "gospelfamily-b59a7.appspot.com",
-  messagingSenderId: "435080268433",
-  appId: "1:435080268433:web:2de91e35c24bb9dedf0d30",
-  measurementId: "G-5DBGYQY8F6"
+ 
 };
 
 // Initialize Firebase
